4935b63bb775381284d27261adebdd19c7d5823d
JYU-Namic <118192387+JYU-Namic@users.noreply.github.com>
2023-06-05 02:26:57 +0000

    suimit in 6/5

--- function.cpp
+++ function.cpp
@@ -1,8 +1,62 @@
+// 【1】预编译
 #include<stdio.h>
+#include<stdlib.h>
 #include<iostream>
+// 【2】宏定义
 using namespace std;
-int main()
+// 【3】结构定义
+typedef struct Lnode
 {
-    cout<<"IDE Test."<<endl;
-    return 0;
+    
+};
+// 【4】功能
+// (1)创建通讯录文件
+int CreatFile()
+{
+
+}
+// (2)添加记录
+int AddRecord()
+{
+
+}
+// (3)删除记录
+int DeleteRecord()
+{
+
+}
+// (4)查找记录
+int FindRecord()
+{
+
+}
+// (5)修改记录
+int ChangeRecord()
+{
+
+}
+// (6)插入记录
+int InsertRecord()
+{
+
+}
+// (7)排序记录
+int RankRecord()
+{
+
+}
+// (8)统计记录
+int StatisticsRecord()
+{
+
+}
+// (9)半记忆查找
+int KMP()
+{
+
+}
+// (10)读取记录
+int ReadRecord()
+{
+
 }
